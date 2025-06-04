25a1671da5f6828d60ecbd4a2178e2c6571a9627
Robert Annett <robannett@gmail.com>
2025-06-04 16:21:43 +0100

    Handle FIX version without delimiters

--- src/main/java/com/rannett/fixplugin/util/FixUtils.java
+++ src/main/java/com/rannett/fixplugin/util/FixUtils.java
@@ -6,6 +6,9 @@ import java.util.regex.Pattern;
 
 public class FixUtils {
 
+    private static final Pattern FIX_VERSION_PATTERN = Pattern.compile(
+            "^(FIX(?:T)?\\.[0-9]+(?:\\.[0-9]+)*)(?:[\\u0001|\\s]|$)");
+
     public static Optional<String> extractFixVersion(String text) {
         int start = text.indexOf("8=FIX");
         if (start == -1) {
@@ -14,9 +17,7 @@ public class FixUtils {
 
         String candidate = text.substring(start + 2);
 
-        Matcher matcher = Pattern
-                .compile("^(FIX(?:T)?\\.[0-9]+(?:\\.[0-9]+)*)(?:[\\u0001|\\s]|$)")
-                .matcher(candidate);
+        Matcher matcher = FIX_VERSION_PATTERN.matcher(candidate);
 
         if (matcher.find()) {
             return Optional.of(matcher.group(1));
