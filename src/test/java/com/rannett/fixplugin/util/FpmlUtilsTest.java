f11a3ba6882a347b676385897baea08077bba314
Robert Annett <robannett@gmail.com>
2025-07-05 15:30:00 +0100

    Allow multiline FpML values

--- src/test/java/com/rannett/fixplugin/util/FpmlUtilsTest.java
+++ src/test/java/com/rannett/fixplugin/util/FpmlUtilsTest.java
@@ -2,7 +2,8 @@ package com.rannett.fixplugin.util;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FpmlUtilsTest {
     @Test
